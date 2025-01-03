efcee62b40800e9590058930cc28a7ce7d4a778a
ChangHoon <nick6253@naver.com>
2025-01-03 15:21:59 +0900

    chore: Add project settings

---
+++ src/test/java/com/ssginc/teamproject/NoJamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssginc.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoJamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
