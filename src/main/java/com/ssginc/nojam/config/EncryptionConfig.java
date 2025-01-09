f99cde51fca27d320d6da8885cb6b9c0ea8c3f79
gunny <gunny97h@gmail.com>
2025-01-09 17:31:28 +0900

    chore(conf): Add sign up password config files (#4)

---
+++ src/main/java/com/ssginc/nojam/config/EncryptionConfig.java
@@ -0,0 +1,9 @@
+package com.ssginc.nojam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:applicationContext.xml")
+public class EncryptionConfig {
+}
