92b82217981c1fc015cb6d13c35e3c130cca9d7d
ChangHoon <nick6253@naver.com>
2025-01-13 09:30:37 +0900

    feat: Add sales by category chart java files (#5)

---
+++ src/main/java/com/ssginc/nojam/chart/mapper/ChartMapper.java
@@ -0,0 +1,17 @@
+package com.ssginc.nojam.chart.mapper;
+
+import com.ssginc.nojam.chart.vo.GetBranchIdAndNameDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ChartMapper {
+
+    List<GetBranchIdAndNameDto> getBranches();
+
+    List<String> getCategoryLabels();
+
+    List<Integer> getSalesByCategory(String branchId);
+
+}
---
+++ src/main/java/com/ssginc/nojam/chart/vo/GetBranchIdAndNameDto.java
@@ -0,0 +1,18 @@
+package com.ssginc.nojam.chart.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetBranchIdAndNameDto {
+
+    private String branchId;
+
+    private String branchName;
+
+}
