2d7137f7c6cabb6793c98de900ea375242a9bfb6
Queue-ri <qriositylog@gmail.com>
2025-01-13 17:25:06 +0900

    feat: Add branch stock mapper files (#25)

--- src/main/java/com/ssginc/nojam/stock/mapper/StockMapper.java
+++ src/main/java/com/ssginc/nojam/stock/mapper/StockMapper.java
@@ -1,12 +1,12 @@
 package com.ssginc.nojam.stock.mapper;
 
 import com.ssginc.nojam.crawl.vo.ItemVO;
+import com.ssginc.nojam.stock.dto.BranchStockViewDTO;
 import com.ssginc.nojam.stock.dto.HeadStockViewDTO;
 import com.ssginc.nojam.stock.vo.BranchStockVO;
 import com.ssginc.nojam.stock.vo.HeadStockVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -24,4 +24,14 @@ public interface StockMapper {
     int countAllHeadStock();
     int countFilteredHeadStock(@Param("category") String category, @Param("value") String value);
     List<HeadStockViewDTO> get50FilteredHeadStock(@Param("category") String category, @Param("value") String value, @Param("startIdx") int startIdx);
+
+    List<BranchStockViewDTO> get50BranchStock(@Param("branchId") String branchId, @Param("startIdx") int startIdx);
+    int countAllBranchStock(@Param("branchId") String branchId);
+    List<BranchStockViewDTO> get50FilteredBranchStock(
+            @Param("branchId") String branchId,
+            @Param("category") String category,
+            @Param("value") String value,
+            @Param("startIdx") int startIdx
+    );
+    int countFilteredBranchStock(@Param("branchId") String branchId, @Param("category") String category, @Param("value") String value);
 }
