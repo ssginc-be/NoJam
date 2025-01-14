05a07465530576091c47e161390a0e037fd57016
gunny <gunny97h@gmail.com>
2025-01-14 10:42:23 +0900

    feat: Add smtp in find pw mapper files (#33)

--- src/main/java/com/ssginc/nojam/member/mapper/MemberMapper.java
+++ src/main/java/com/ssginc/nojam/member/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.ssginc.nojam.member.mapper;
 
 import com.ssginc.nojam.member.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.ui.Model;
 
 import java.util.List;
@@ -13,4 +14,9 @@ public interface MemberMapper {
 
     MemberVO selectMemberById(String userId);
 
+    // 비밀번호 찾기
+    int updatePasswordByEmail(@Param("userEmail") String userEmail, @Param("userPw") String userPw);
+
+    // 이메일로 회원 조회
+    MemberVO selectMemberByEmail(@Param("userEmail") String userEmail);
 }
